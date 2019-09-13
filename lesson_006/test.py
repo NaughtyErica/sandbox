916af52cbc2ded07d72b86cd8e29b6877471f436
kv2709@gmail.com <Av5ohsho2709>
2019-09-13 19:04:41 +0700

    test file hash

--- lesson_006/test.py
+++ lesson_006/test.py
@@ -1,28 +1,14 @@
-import pygame
+import hashlib
+str_to_crypt = 'Начинаем игру "Быки и Коровы". Я задумываю четырехзначное число, ' \
+               'все цифры которого различны (первая цифра числа отлична от нуля).' \
+               'Вам необходимо разгадать задуманное число. Вы вводите четырехзначное ' \
+               'число c неповторяющимися цифрами, я сообщаю о количестве «быков» и «коров» ' \
+               'в названном числе. «Бык» — цифра есть в записи задуманного числа и стоит ' \
+               'в той же позиции, что и в задуманном числе, «Корова» — цифра есть в записи' \
+               'задуманного числа, но не стоит в той же позиции, что в задуманном числе.'
+
+print(hashlib.sha3_512(str_to_crypt.encode('utf-8')).hexdigest())
 
 
-
-pygame.init()
-
-running = True
-clock = pygame.time.Clock()
-screen_size = (100, 100)
-screen = pygame.display.set_mode(screen_size)
-my_color = [250, 250, 100]
-
-while running:
-    # здесь смотрим,какие события произошли
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:  # нажатие на крестик: выход
-            running = False
-        if event.type == pygame.KEYDOWN:  # тип события - нажатие на кнопку
-            if event.key == pygame.K_w:  # нажатие на w
-                print("w")
-            if event.key == pygame.K_s:  # нажатие на s
-                print("s")
-
-    screen.fill(my_color)
-    pygame.display.flip()
-    clock.tick(30)
-
-pygame.quit()
+# 85d85ee3b565a4acd756169aec695102afda7534fbbfa6a7d591516ec91adb915d4981f4354204c1bb03f514eb6bf7214948f53bc3767823522e96ed685f6999
+# 93061fdc4a581b01aeef3f9a6539c01eaa7a6fb4318fa0af41435af350ce9d18b0df2847974b162289a92d661dc29419d609e5858402f1499a4965b74ca5a7db
