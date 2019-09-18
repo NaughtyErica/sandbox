1cb89aa9f58d0d9ccc432824c59b1b42c3db6de0
kv2709@gmail.com <Av5ohsho2709>
2019-09-18 09:00:33 +0700

    update

--- lesson_006/mastermind_engine.py
+++ lesson_006/mastermind_engine.py
@@ -7,17 +7,14 @@ def pick_number():
     global search_number_lst
     search_number_lst = []
     list_select = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    step = 0
+    step = 1
     while len(search_number_lst) <= 3:
-        step += 1
         number = rd.choice(list_select)
-        if number != '0' and step == 1:
+        step += 1
+        if number != '0' or step != 1:
             list_select.remove(number)
             search_number_lst.append(number)
-        elif step > 1:
-            list_select.remove(number)
-            search_number_lst.append(number)
-    print('Загаданное число', search_number_lst)
+#    print('Загаданное число', search_number_lst)
 
 
 def check_number(user_input_str=''):
@@ -25,16 +22,12 @@ def check_number(user_input_str=''):
     answer = {}
     user_number_lst = list(user_input_str)
     bulls = 0
-    cows = 0
     for i in range(len(user_number_lst)):
         if user_number_lst[i] == search_number_lst[i]:
             bulls += 1
-            user_number_lst[i] = 'b'
-    else:
-        for i in range(len(user_number_lst)):
-            if user_number_lst[i] in search_number_lst:
-                cows += 1
-                user_number_lst[i] = 'c'
+    set_search = set(search_number_lst)
+    set_user = set(user_number_lst)
+    cows = len(set_search & set_user) - bulls
     answer['bulls'] = bulls
     answer['cows'] = cows
     return answer
