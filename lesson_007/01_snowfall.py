f2d64b4763840e67f0572834cdccafd767cf1ade
kv2709 <kv2709@gmail.com>
2019-10-02 14:49:29 +0700

    Делаю очередной снегопад в объектах

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -40,9 +40,9 @@ class Snowflake:
         self.center_and_beams_snowflake.append(factor_c)
         Snowflake.quantity_snow_flakes += 1
 
-    def __del__(self):
-        Snowflake.quantity_snow_flakes -= 1
-
+    # def __del__(self, position=0):
+    #     Snowflake.quantity_snow_flakes -= 1
+    #     Snowflake.list_snow_flakes.pop(position)
 
     def draw_snow_flake(self, color=sd.COLOR_YELLOW):
         """
@@ -64,7 +64,8 @@ class Snowflake:
 
     @classmethod
     def create_list_snow_flakes(cls, quantity=0):
-        for i in range(quantity):
+        Snowflake.quantity_snow_flakes = quantity
+        for fl in range(quantity):
             Snowflake.list_snow_flakes.append(cls())
 
     @classmethod
@@ -74,11 +75,23 @@ class Snowflake:
     @classmethod
     def list_flown_bottom(cls):
         Snowflake.number_snowflake_bottom = []
-        for i in range(Snowflake.quantity_snow_flakes):
-            if Snowflake.list_snow_flakes[i][1] < 0 - 35:
-                Snowflake.number_snowflake_bottom.append(i)
+        for fl in range(Snowflake.quantity_snow_flakes):
+            if Snowflake.list_snow_flakes[fl][1] < 0 - 35:
+                Snowflake.number_snowflake_bottom.append(fl)
         return Snowflake.number_snowflake_bottom
 
+    @classmethod
+    def delete_flown_bottom(cls):
+        quantity_delete = len(Snowflake.number_snowflake_bottom)
+        if quantity_delete > 0:
+            for fl in range(quantity_delete):
+                del Snowflake.list_snow_flakes[Snowflake.number_snowflake_bottom[fl]]
+                Snowflake.list_snow_flakes.insert(Snowflake.number_snowflake_bottom[fl], cls())
+
+    @classmethod
+    def draw_all_snow_flakes(cls, color=sd.COLOR_YELLOW):
+        for fl in range(Snowflake.quantity_snow_flakes):
+            cls.draw_snow_flake()
 
 
     # def del_snow_flake(self, number_snow_flake=0):
@@ -115,7 +128,6 @@ class Snowflake:
     #     elif direction < 0:
     #         print("Ветер справа")
 
-
 # flake = Snowflake()
 #
 # while True:
@@ -142,4 +154,39 @@ class Snowflake:
 #     if sd.user_want_exit():
 #         break
 
+count_snow_flakes = 0
+resolution_screen = sd.resolution = (1200, 600)
+Snowflake.create_list_snow_flakes(quantity=20)
+# print("Усиление снегопада - S")
+# print("Уменьшение снегопада - W")
+# print("Ветер справа - A")
+# print("Ветер слева - D")
+# print("Клавиши нажимать дробно. Усиление +5 снежинок, уменьшение -1")
+while True:
+    sd.start_drawing()
+    snow.draw_snow_flakes(color=sd.background_color)
+    snow.shift_coord_snow_flakes()
+    snow.draw_snow_flakes(color=sd.COLOR_WHITE)
+    lst_bottom = snow.list_flown_bottom()
+    quantity_deleted = len(lst_bottom)
+    if quantity_deleted > 0:
+        for i in range(quantity_deleted):
+            snow.del_snow_flake(number_snow_flake=lst_bottom[i])
+            snow.create_snowflakes_pos(quantity=1, position_add=lst_bottom[i], resolution=resolution_screen)
+    sd.finish_drawing()
+    # В таком вариант у меня устойчиво ловит команды клавиатуры, правда иногда дублирует их
+    # а в предыдущем варианте почти не работало
+    keyState = pygame.key.get_pressed()
+    if keyState[pygame.K_s]:
+        count_snow_flakes = snow.create_snowflakes(quantity=5, resolution=resolution_screen)
+    if keyState[pygame.K_w]:
+        snow.thin_snow()
+    if keyState[pygame.K_a]:
+        snow.wind(direction=-1)
+    if keyState[pygame.K_d]:
+        snow.wind(direction=1)
+    sd.sleep(0.1)
+    if sd.user_want_exit() or count_snow_flakes > 200:
+        break
 sd.pause()
+
