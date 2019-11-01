c33f4679c0921d40265b5064cffcd077ccf857ca
kv2709 <kv2709@gmail.com>
2019-11-01 15:59:00 +0700

    Добавил девятый модуль

--- yandex_olimp/test_feature.py
+++ yandex_olimp/test_feature.py
@@ -1,16 +1,5 @@
-class MyStr(str):
-    def __init__(self, string=''):
-        super().__init__()
-        self.string = string
-
-    def insert_into_pos(self, position=0, source=''):
-        res = list(self.string)
-        res.insert(position, source)
-        return ''.join(res)
 
 
-sss = MyStr('privetik')
-ins_sss = sss.insert_into_pos(3, '89898')
-upper_sss = sss.upper()
-print(ins_sss)
-print(upper_sss)
+for i in range(2000, 3000):
+    print(chr(i))
+
