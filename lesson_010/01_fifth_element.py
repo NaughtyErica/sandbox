98f0602a2b833fab1f0d2da97a458f691eac0ad3
kv2709 <kv2709@gmail.com>
2019-11-22 20:14:18 +0700

    Третья задачка по 10-му модулю

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -11,18 +11,15 @@ except ValueError:
     print(f'{input_data[4]} не является числом!')
 except IndexError:
     print('Нет пятого элеменнта в веденном списке!')
-except:
-    print('Ошибка ввода!')
+except EOFError as exc:
+    print(f'Неожиданная ошибка {exc}!')
 else:
     result = BRUCE_WILLIS * lee_loo
     print(f'- Leeloo Dallas! Multi-pass № {result}!')
+
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
 # - IndexError - выход за границы списка
 # - остальные исключения EOFError
 # для каждого типа исключений написать на консоль соотв. сообщение
-
-
-
-
